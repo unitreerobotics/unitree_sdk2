de417d99367bb9c03a7c9af2176c034264775385
Manan Gandhi <gamanan@amazon.com>
2025-11-18 21:25:13 -0800

    Fix build_wheels.sh to work from deploy directory

--- deploy/build_wheels.sh
+++ deploy/build_wheels.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# Change to repo root if running from deploy directory
+if [ "$(basename $(pwd))" = "deploy" ]; then
+    cd ..
+fi
+
 UNITREE_TAG=$(grep '^version = ' pyproject.toml | cut -d'"' -f2)
 PYTHON_VERSIONS=${PYTHON_VERSIONS:-"3.8 3.10"}
 ARCHITECTURES=${ARCHITECTURES:-"x86_64 aarch64"}
@@ -29,16 +34,16 @@ ARG UNITREE_TAG
 ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get update && apt-get install -y \\
     build-essential cmake git python3-pip \\
-    python3-pybind11 pybind11-dev patchelf wget curl \\
+    patchelf wget curl \\
     libssl-dev libffi-dev libbz2-dev libreadline-dev libsqlite3-dev \\
     libncurses5-dev libncursesw5-dev xz-utils tk-dev libgdbm-dev \\
     libc6-dev libnss3-dev zlib1g-dev && \\
     rm -rf /var/lib/apt/lists/*
 
-# Install Python 3.8 (already available) or compile Python 3.10 from source
+# Install Python 3.8 (already available) or compile other versions from source
 RUN if [ "\$PYTHON_VER" = "3.8" ]; then \\
         apt-get update && apt-get install -y python3.8 python3.8-dev python3.8-distutils && rm -rf /var/lib/apt/lists/*; \\
-    else \\
+    elif [ "\$PYTHON_VER" = "3.10" ]; then \\
         cd /tmp && \\
         wget https://www.python.org/ftp/python/3.10.12/Python-3.10.12.tgz && \\
         tar xzf Python-3.10.12.tgz && \\
@@ -48,6 +53,16 @@ RUN if [ "\$PYTHON_VER" = "3.8" ]; then \\
         make altinstall && \\
         ln -sf /usr/local/bin/python3.10 /usr/bin/python3.10 && \\
         cd / && rm -rf /tmp/Python-3.10.12*; \\
+    elif [ "\$PYTHON_VER" = "3.11" ]; then \\
+        cd /tmp && \\
+        wget https://www.python.org/ftp/python/3.11.9/Python-3.11.9.tgz && \\
+        tar xzf Python-3.11.9.tgz && \\
+        cd Python-3.11.9 && \\
+        ./configure --enable-optimizations --prefix=/usr/local && \\
+        make -j\$(nproc) && \\
+        make altinstall && \\
+        ln -sf /usr/local/bin/python3.11 /usr/bin/python3.11 && \\
+        cd / && rm -rf /tmp/Python-3.11.9*; \\
     fi
 
 RUN python\$PYTHON_VER -m pip install --upgrade pip setuptools wheel build pybind11
@@ -59,7 +74,9 @@ RUN rm -rf unitree_interface/*.so* build_py\$PY_TAG dist && \\
     mkdir -p build_py\$PY_TAG && cd build_py\$PY_TAG && \\
     cmake .. -DBUILD_PYTHON_BINDING=ON -DCMAKE_BUILD_TYPE=Release \\
     -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON -DCMAKE_INSTALL_RPATH='\$ORIGIN' \\
-    -DPYTHON_EXECUTABLE=/usr/bin/python\$PYTHON_VER && \\
+    -DPYTHON_EXECUTABLE=/usr/bin/python\$PYTHON_VER \\
+    -Dpybind11_DIR=\$(python\$PYTHON_VER -c "import pybind11; print(pybind11.get_cmake_dir())") \\
+    -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=OFF && \\
     make -j\$(nproc) && cd .. && \\
     find build_py\$PY_TAG -name "unitree_interface.cpython-*.so" -exec cp {} unitree_interface/ \\; && \\
     cp thirdparty/lib/\$ARCH/*.so* unitree_interface/ && \\
