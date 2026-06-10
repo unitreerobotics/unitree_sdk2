1a16684f32fb75dfd9c0aace012fc8126fb9794e
zhoujingbo813 <zhoujingbo1388@126.com>
2026-06-10 01:02:21 -0700

    add r1 audio client

---
+++ include/unitree/robot/r1/audio/audio_error.hpp
@@ -0,0 +1,14 @@
+#ifndef __UT_ROBOT_R1_AUDIO_ERROR_HPP__
+#define __UT_ROBOT_R1_AUDIO_ERROR_HPP__
+
+#include <unitree/common/decl.hpp>
+
+namespace unitree {
+namespace robot {
+namespace r1 {
+UT_DECL_ERR(UT_ROBOT_AUDIO_ERR_COMM, 100, "Invalid parameter ")
+}  // namespace r1
+}  // namespace robot
+}  // namespace unitree
+
+#endif  // __UT_ROBOT_R1_AUDIO_ERROR_HPP__
